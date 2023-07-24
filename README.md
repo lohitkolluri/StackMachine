7404ff1dcf6b2c992468c5923e841172b651bf62
lohit <lohitkolluri@gmail.com>
2023-07-24 12:54:47 +0530

    Some code changes and README.md

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# StackMachine
+# Stack Machine in Python
+
+This repository contains a simple implementation of a stack machine in Python. A stack machine is a type of computer processor design that relies on a stack to hold values, rather than a set of registers. This stack machine supports basic arithmetic operations and stack operations.
+
+## Usage
+
+Create an instance of the `StackMachine` class and call its `execute` method with each instruction in the program.
+
+Example:
+
+```python
+sm = StackMachine()
+sm.execute("PUSH 5")
+sm.execute("PUSH 3")
+sm.execute("ADD")
+sm.execute("PRINT")  # Outputs: 8
+```
+
+In this example, the program pushes `5` and `3` onto the stack, adds them together (removing them from the stack and pushing the result onto the stack), and then prints the result.
+
+## Instructions
+
+The following instructions are supported:
+
+- `PUSH <n>`: Pushes the number `<n>` onto the stack.
+- `POP`: Removes the top value from the stack and discards it.
+- `ADD`: Removes the top two values from the stack, adds them together, and pushes the result back onto the stack.
+- `SUB`: Removes the top two values from the stack, subtracts the second one from the first, and pushes the result back onto the stack.
+- `MUL`: Removes the top two values from the stack, multiplies them together, and pushes the result back onto the stack.
+- `DIV`: Removes the top two values from the stack, divides the second one by the first (throwing an error if the first is zero), and pushes the result back onto the stack.
+- `PRINT`: Prints the top value on the stack.
+
+Each instruction is executed by calling the `execute` method of the `StackMachine` class with the instruction as a string.
+
+## Testing
+
+A simple test program is included in the `stack_machine.py` file. This program demonstrates basic usage of the stack machine.
+
+To run the test program, execute the `stack_machine.py` file using a Python interpreter.
+
+## Requirements
+
+Python 3.6 or higher is required to run this program.
--- stack_machine.py
+++ stack_machine.py
@@ -6,37 +6,70 @@ class StackMachine:
         self.stack.append(value)
 
     def pop(self):
+        if not self.stack:
+            raise RuntimeError("Attempted to pop an empty stack")
         return self.stack.pop()
 
     def add(self):
-        a = self.pop()
-        b = self.pop()
-        self.push(a + b)
+        if len(self.stack) < 2:
+            raise RuntimeError("Attempted to add with less than two values on the stack")
+        a = self.stack.pop()
+        b = self.stack.pop()
+        self.stack.append(a + b)
 
-    def sub(self):
-        a = self.pop()
-        b = self.pop()
-        self.push(a - b)
+    def subtract(self):
+        if len(self.stack) < 2:
+            raise RuntimeError("Attempted to subtract with less than two values on the stack")
+        a = self.stack.pop()
+        b = self.stack.pop()
+        self.stack.append(b - a)
 
-    def mul(self):
-        a = self.pop()
-        b = self.pop()
-        self.push(a * b)
+    def multiply(self):
+        if len(self.stack) < 2:
+            raise RuntimeError("Attempted to multiply with less than two values on the stack")
+        a = self.stack.pop()
+        b = self.stack.pop()
+        self.stack.append(a * b)
 
-    def div(self):
-        a = self.pop()
-        b = self.pop()
-        self.push(a // b)
+    def divide(self):
+        if len(self.stack) < 2:
+            raise RuntimeError("Attempted to divide with less than two values on the stack")
+        a = self.stack.pop()
+        b = self.stack.pop()
+        if a == 0:
+            raise RuntimeError("Attempted to divide by zero")
+        self.stack.append(b / a)
 
-    def halt(self):
-        exit()
+    def print(self):
+        if not self.stack:
+            raise RuntimeError("Attempted to print an empty stack")
+        print(self.stack[-1])
 
-def main():
-    machine = StackMachine()
-    machine.push(1)
-    machine.push(2)
-    machine.add()
-    print(machine.pop())
+    def execute(self, instruction):
+        if instruction == "ADD":
+            self.add()
+        elif instruction == "SUB":
+            self.subtract()
+        elif instruction == "MUL":
+            self.multiply()
+        elif instruction == "DIV":
+            self.divide()
+        elif instruction == "PRINT":
+            self.print()
+        elif instruction.startswith("PUSH"):
+            _, num = instruction.split()
+            self.push(int(num))
+        elif instruction == "POP":
+            self.pop()
+        else:
+            raise RuntimeError(f"Unrecognized instruction: {instruction}")
 
-if __name__ == "__main__":
-    main()
+
+def test_program():
+    sm = StackMachine()
+    sm.execute("PUSH 5")
+    sm.execute("PUSH 3")
+    sm.execute("ADD")
+    sm.execute("PRINT")  # Should print 8
+
+test_program()
